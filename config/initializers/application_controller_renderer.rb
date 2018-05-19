251079114005fe9d40ff942ca3e632c923f25b6d
seona-yu <seona@likelion.org>
2018-05-19 15:15:53 +0900

    선아

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -8,19 +8,19 @@
 # end
 
 
-var myData={
-    "Apple": null,
-    "Microsoft": null,
-    "Google": 'https://placehold.it/500x500'
-  };
+# var myData={
+#   "Apple": null,
+#   "Microsoft": null,
+#   "Google": 'https://placehold.it/250x250'
+# };
 
-$(document).ready(function() {
-$('input.autocomplete').autocomplete({
-  data: myData,
-  limit: 20, // The max amount of results that can be shown at once. Default: Infinity.
-  onAutocomplete: function(val) {
-    // Callback function when value is autcompleted.
-  },
-  minLength: 1, // The minimum length of the input for the autocomplete to start. Default: 1.
-});
-});
+# $(document).ready(function() {
+# $('input.autocomplete').autocomplete({
+# data: myData,
+# limit: 20, // The max amount of results that can be shown at once. Default: Infinity.
+# onAutocomplete: function(val) {
+#   // Callback function when value is autcompleted.
+# },
+# minLength: 1, // The minimum length of the input for the autocomplete to start. Default: 1.
+# });
+# });
